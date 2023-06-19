35bc09498565aa4b9d566c1b00bb4b9e58a41a57
Tiago Pereira <tpereira@c2r3s5.42lisboa.com>
2023-06-19 20:32:34 +0100

    working on ex02

---
+++ ex02/includes/RobotomyRequestForm.hpp
@@ -0,0 +1,27 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+
+# include <iostream>
+# include "AForm.hpp"
+# include <cstdlib>
+
+class RobotomyRequestForm: public AForm
+{
+	private:
+		std::string _target;
+
+	public:
+
+		RobotomyRequestForm();
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm( RobotomyRequestForm const & src );
+		~RobotomyRequestForm();
+
+		RobotomyRequestForm &		operator=( RobotomyRequestForm const & rhs );
+
+		virtual void	formFunction() const;
+};
+
+std::ostream &			operator<<( std::ostream & o, RobotomyRequestForm const & i );
+
+#endif /* ********************************************* ROBOTOMYREQUESTFORM_H */
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,18 +6,19 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 10:45:29 by tpereira          #+#    #+#             */
-/*   Updated: 2023/05/14 11:54:29 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/06/19 20:32:05 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main()
 {
-	ShrubberyCreationForm test("Form1");
-	ShrubberyCreationForm test2("TESTS");
+	ShrubberyCreationForm test("Shrubbery");
+	RobotomyRequestForm test2("Robotomy");
 
 	test.formFunction();
 	test2.formFunction();
